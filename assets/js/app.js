eb46c8317faed99fcc7ddc92eedae132e15da466
KarenAedo <karen.aedo.mayorga@gmail.com>
2018-01-11 13:33:01 -0300

    Dejando todo en un solo html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,38 @@
 $(document).ready(function(){
-	
-	//Splash
-   	setTimeout(function(){
-   		$('#splash').fadeOut(500);
-   	}, 2000);
-   
+	}); //.ready
+
+
+  $(function(){
+   setTimeout(function() {
+     $('#splash').fadeOut(500);
+   }, 1000);
+  });
+  $(function(){
+    setTimeout(function() {
+      $('.firstSection').removeClass('hidden');
+    }, 1000);
+  });
+
+ 	$('#btn-singup').click(function(){
+  	$('.firstSection').addClass('hidden');
+  	$('.signUp').removeClass('hidden');
+  });
+
+ 	$('#btn-singup-2').click(function(){
+  	$('.signUp').addClass('hidden');
+  	$('.verifyPhone').removeClass('hidden');
+  });
+
+  $('#btn-verify').click(function(){
+  	$('.verifyPhone').addClass('hidden');
+  	$('.signUpTwo').removeClass('hidden');
+  });
+
+  $('#btn-signUpTwo').click(function(){
+  	$('.signUpTwo').addClass('hidden');
+  	$('.finalOk').removeClass('hidden');
+  });
 
 
 
-}); //.ready
+
